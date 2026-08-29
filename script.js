db5bb58484754a22bd884c20eb8bc2f3a418ed47
Githuperson <Kkminerishere@gmail.com>
2026-08-29 20:01:08 +0100

    Update API_URL to new endpoint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://libre-ui.kkminerishere.workers.dev/api.php";
+const API_URL = "https://libre-ui-api.kkminerishere.workers.dev/api.php";
 
 const form = document.getElementById("searchForm");
 const input = document.getElementById("searchInput");
